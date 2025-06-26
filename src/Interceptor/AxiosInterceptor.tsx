d1c8bc48fb377aa6d3b8c07fac4d3128031bc6a6
Charsobis420 <hirenparmar0420@gmail.com>
2025-06-26 15:14:58 +0530

    Added Backend URL

--- src/Interceptor/AxiosInterceptor.tsx
+++ src/Interceptor/AxiosInterceptor.tsx
@@ -1,7 +1,8 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080'
+    // baseURL: 'http://localhost:8080'
+    baseURL: 'https://jobportal-backend-x7cl.onrender.com'
 })
 
 axiosInstance.interceptors.request.use(
